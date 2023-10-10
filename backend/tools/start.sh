8941cb43f6d1c02b507ca1f569d9adab967306ea
Knosper <j.jesberger@yahoo.com>
2023-10-10 14:24:51 +0200

    backend service is restarting?!

--- backend/tools/start.sh
+++ backend/tools/start.sh
@@ -1,16 +1,1 @@
-#TODO make a scipt as entrypoint for npm
-#because there are errors like:
-
-#npm ERR! Missing script: "start"
-#npm ERR! 
-#npm ERR! Did you mean one of these?
-#npm ERR!     npm star # Mark your favorite packages
-#npm ERR!     npm stars # View packages marked as favorites
-#npm ERR! 
-#npm ERR! To see a list of scripts, run:
-#npm ERR!   npm run
-
-#npm ERR! A complete log of this run can be found in:
-#npm ERR!     /root/.npm/_logs/2023-10-05T13_34_25_531Z-debug-0.log
-
 echo "hello World"
