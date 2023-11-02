bd0e73ea87e67ea13aff6ec6612db354b155eaf7
Knosper <j.jesberger@yahoo.com>
2023-11-02 13:00:33 +0100

    localhost:3000 works

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-59=-u(21+chk%rhau&67g)45#w8zahj#&*qhsms^y(zo4nju4-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
