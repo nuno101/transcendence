7610e724e227780a856ac4add8eb849ec8ab3db5
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-11-16 15:32:39 +0100

    implemented multiple language support

---
+++ frontend/src/components/HomeSelection.vue
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<template>
+    <h1 class="btn-primary">THIS IS THE HOME PAGE</h1>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
+import i18n from './plugins/i18n';
 
 const app = createApp(App);
 
 // Use the router
 app.use(router);
+
+// Use i18n for languages
+app.use(i18n);
 
 // Mount the app to the DOM
 app.mount('#app');
