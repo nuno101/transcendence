e071306653128900b0d8f9e9994dccdf3e205b5d
Nuno <nlouro@student.42heilbronn.de>
2023-11-22 15:05:54 +0100

    users index and show REST actions working

--- backend/transc/api/views.py
+++ backend/transc/api/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from django.core.serializers import serialize
 from .models import User
-import json
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the transcendence index.")
@@ -9,17 +8,21 @@ def index(request):
 def test(request):
 	return JsonResponse({'foo':'bar'})
 
-def users(request):
-	users = User.objects.order_by("name")
-	data = serialize('json', list(users), fields=('id','name','fullname'))
-	return JsonResponse({'users': data})
+def user_list(request):
+	if request.method == 'GET':
+		users = User.objects.order_by("name")
+		data = serialize('json', list(users), fields=('id','name','fullname'))
+		return JsonResponse({'users': data})
+
+	if request.method == 'POST':
+		# https://www.youtube.com/watch?v=i5JykvxUk_A
+		return JsonResponse({'TODO':'1'})
 
 #FIXME
-def show(request, user_id):
-	##user = get_object_or_404(User, pk=user_id)
+def user_detail(request, user_id):
 	try:
 		user = User.objects.get(pk=user_id)
-		data = serialize('json', list(user), fields=('id','name','fullname'))
+		data = serialize('json', [user], fields=('id','name','fullname','created_at', 'updated_at'))
 	except User.DoesNotExist:
 		raise Http404()
 	return JsonResponse({'user': data})
