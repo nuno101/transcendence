ef342a8c044bbf1414207ec564bd6d997e0e4af8
Nuno <nlouro@student.42heilbronn.de>
2023-11-22 14:32:21 +0100

    GET /users working and returning JSON list of users

--- backend/transc/api/urls.py
+++ backend/transc/api/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("test", views.test, name="test"),
+	path("", views.index, name="index"),
+	path("test", views.test, name="test"),
+	path("users", views.users, name="users"),
+	path("<int:user_id>/", views.show, name="show"),
 ]
 
