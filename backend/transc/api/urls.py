920b617d1e94e04f8e387e05613d99a255737035
Nuno <nlouro@student.42heilbronn.de>
2023-11-24 12:17:41 +0100

    GET /tournaments working but using different code architecture. POST /tournaments requires CSRF token atm

--- backend/transc/api/urls.py
+++ backend/transc/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
 from . import views
+from .views import TournamentView
 
 urlpatterns = [
 	path("", views.index, name="index"),
-	path("test", views.test, name="test"),
 	path("users", views.user_list),
+	path("users/", views.user_list),
 	path("users/<int:user_id>", views.user_detail),
+	path('tournaments/', TournamentView.as_view()),
 ]
 
--- backend/transc/api/views.py
+++ backend/transc/api/views.py
@@ -1,18 +1,16 @@
+from django.views import View
 from django.http import HttpResponse, JsonResponse, Http404
 #from django.core.serializers import serialize
 from .models import User
+from .models import Tournament
+#from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the transcendence index.")
 
-def test(request):
-	return JsonResponse({'foo':'bar'})
-
 def user_list(request):
 	if request.method == 'GET':
 		users = User.objects.order_by("name")
-		#data = serialize('json', list(users), fields=('id','name','fullname'))
-        # '[{"model": "api.user", "pk": 2, "fields": {"name": "dummy", "fullname": "The Dummy"}}, {...}
 		users_data = []
 		for user in users:
 			users_data.append({
@@ -33,7 +31,6 @@ def user_list(request):
 def user_detail(request, user_id):
 	try:
 		user = User.objects.get(pk=user_id)
-		#data = serialize('json', [user], fields=('id','name','fullname','created_at', 'updated_at'))
 		data = {'id': user.id,
 				'name': user.name,
 				'fullname': user.fullname,
@@ -41,3 +38,45 @@ def user_detail(request, user_id):
 	except User.DoesNotExist:
 		raise Http404()
 	return JsonResponse({'user': data})
+
+#@csrf_exempt
+class TournamentView(View):
+	def post(self, request):
+		data = json.loads(request.body.decode("utf-8"))
+		title = data.get('title')
+		description = data.get('description')
+		creator_id = data.get('creator_id')
+
+		tournament_data = {
+			'title': title,
+			'description': description,
+			'creator_id': creator_id,
+			#'status': TournamentStatus::CREATED
+			'created_at': datetime.datetime.now()
+		}
+
+		tournament_item = Tournament.objects.create(**tournament_data)
+
+		data = {
+			"message": "New item added to Cart with id: {tournament_item.id}"
+		}
+		return JsonResponse(data, status=201)
+
+	def get(self, request):
+		tournaments = Tournament.objects.all()
+		tournaments_data = []
+		for t in tournaments:
+			tournaments_data.append({
+				'id': t.id,
+				'title': t.title,
+				'description': t.description,
+				'creator_id': t.creator_id,
+				#'status': t.status,
+				'created_at': t.created_at,
+			})
+		data = {
+			'tournaments': tournaments_data,
+			'count': tournaments.count(),
+		}
+		return JsonResponse(data)
+
