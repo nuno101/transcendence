2073d2ec17e0c6173babf568ea70252470111136
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-11-28 19:45:39 +0100

    svg added & worked on onsite game

--- frontend/src/views/CreateProfileView.vue
+++ frontend/src/views/CreateProfileView.vue
@@ -7,8 +7,13 @@ import { useI18n } from 'vue-i18n';
     <h1>{{useI18n().t('createprofileview.createprofile')}}</h1>
     <h3>{{useI18n().t('createprofileview.nickname')}}</h3>
     <p>(must be unique)</p>
+    <input type="text" class="form-control" id="nickname">
     <h3>{{useI18n().t('createprofileview.uploadavatar')}}</h3>
     <p>(default option must be provided)</p>
+    <div class="mb-3">
+      <label for="formFile" class="form-label">Upload your avatar</label>
+      <input class="form-control" type="file" id="formFile" accept="image/*">
+    </div>
     <router-link to="/">
       <button type="button" class="btn btn-success">{{useI18n().t('createprofileview.createaccount')}}</button>
     </router-link>  </div>
--- frontend/src/views/PongGameView.vue
+++ frontend/src/views/PongGameView.vue
@@ -1,29 +1,10 @@
-<template>
-  <div class="jumping-ball">
-    <svg
-      width="100"
-      height="100"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 100"
-    >
-      <circle cx="50" cy="50" r="15" fill="blue" />
-    </svg>
-  </div>
-</template>
-
 <script setup>
+import PongGame from '../components/game/PongGame.vue';
 </script>
 
-<style scoped>
-/* Add styles for the jumping ball here if needed */
-.jumping-ball {
-  position: relative;
-  animation: jump 1s infinite alternate;
-}
+<template>
+  <PongGame/>
+</template>
 
-@keyframes jump {
-  to {
-    transform: translateY(-20px);
-  }
-}
+<style scoped>
 </style>
