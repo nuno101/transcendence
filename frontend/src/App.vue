4149bb1783f297cd7b29b6dadd0d584d3450905f
Nuno <nlouro@student.42heilbronn.de>
2023-12-03 22:33:35 +0100

    Place all pages with a Bootstrap container

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import LanguageSelection from './components/common/LanguageSelection.vue';
+  import { RouterLink, RouterView } from 'vue-router';
+  import LanguageSelection from './components/common/LanguageSelection.vue';
 </script>
 
 <template>
-<!-- add things here that should be visible on ALL sites -->
+  <!-- add things here that should be visible on ALL sites -->
+  <div class="container py-4 px-3 mx-auto">
     <LanguageSelection />
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
