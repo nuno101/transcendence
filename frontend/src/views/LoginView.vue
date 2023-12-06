9c89ef5aeb3edec67d3d5c5ca751a7c6b3c0831b
jmatheis00 <jmatheis@student.42heilbronn.de>
2023-12-06 16:24:17 +0100

    GET request frontend working

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,10 +1,35 @@
 <script setup>
-  import { useI18n } from 'vue-i18n';
+import { useI18n } from 'vue-i18n';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const users = ref([]);  // Initialize as a reactive ref
+const fetchData = async () => {
+  try {
+    const response = await axios.get('/api/users');
+    console.log(response.data); // Log the response data to the console
+    users.value = response.data.users; // Fill users variable with values  
+    return response.data;  // Return the data to be used in the template
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+onMounted(() => {
+  fetchData();  // Call fetchData when the component is mounted
+});
 </script>
 
 <template>
   <div>
     <h1>{{useI18n().t('loginview.login')}}</h1>
+    <!-- PRINTING ALL USERS TO PAGE ON BROWSER -->
+    <!-- <div v-for="user in users" :key="user.id">
+      <h2>User Information:</h2>
+      <p><strong>ID:</strong> {{ user.id }}</p>
+      <p><strong>Name:</strong> {{ user.name }}</p>
+      <p><strong>Fullname:</strong> {{ user.fullname }}</p>
+    </div> -->
     <h3>42 API</h3>
     <p>or</p>
     <h3>GOOGLE</h3>
@@ -22,3 +47,4 @@
 
 <style>
 </style>
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,16 +10,21 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // Your Django backend URL
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // Remove the '/api' prefix when forwarding the request
+      }
+    }
   }
-  // ,server: {
-  //   port: 8080,
-  //   host: 'localhost',
-  // }
 })
