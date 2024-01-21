309cc62fed25576a95451c02da2f242d1d9409c8
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-01-22 00:33:33 +0100

    Websocket consumer code cleanup

--- backend/transc/transc/consumers.py
+++ backend/transc/transc/consumers.py
@@ -10,11 +10,12 @@ class Consumer(AsyncWebsocketConsumer):
         self.user = self.scope.get('user')
         if self.user.is_anonymous:
             await self.close()
+        self.channel_name = f'user_{self.user.id}'
 
-        # Add user to user specific group
+        # Add consumer to user specific group
         await self.add_group({'group': f'user_{self.user.id}'})
 
-        # Add user to channel specific groups
+        # Add consumer to channel specific groups
         user_channels = await sync_to_async(self.get_user_channels)()
         for channel in user_channels:
             await self.add_group({'group': f'channel_{channel.id}'})
@@ -25,10 +26,10 @@ class Consumer(AsyncWebsocketConsumer):
         if self.user.is_anonymous:
             return
 
-        # Remove user from user specific group
+        # Remove consumer from user specific group
         await self.remove_group({'group': f'user_{self.user.id}'})
 
-        # Remove user from channel specific groups
+        # Remove consumer from channel specific groups
         user_channels = await sync_to_async(self.get_user_channels)()
         for channel in user_channels:
             await self.remove_group({'group': f'channel_{channel.id}'})
@@ -39,27 +40,21 @@ class Consumer(AsyncWebsocketConsumer):
             event = data['event']
             data = data['data']
         except:
-            await self.send(text_data=json.dumps({
-                'event': 'error',
-                'data': {
-                    'message': 'Invalid data format'
-                }
-            }))
-        
+            await self.send_error('Invalid data')
+            return
+
         if event == 'ping':
             await self.send(text_data=json.dumps({
                 'event': 'pong',
                 'data': {}
             }))
         else:
-            await self.send(text_data=json.dumps({
-                'event': 'error',
-                'data': {
-                    'message': 'Invalid event'
-                }
-            }))
+            await self.send_error('Invalid event')
 
     # Group methods
+
+    async def send_notification(self, event):
+        await self.send(text_data=json.dumps(event['data']))
 
     async def add_group(self, event):
         await self.channel_layer.group_add(
@@ -73,10 +68,15 @@ class Consumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-    async def send_notification(self, event):
-        await self.send(text_data=json.dumps(event['data']))
-
     # Helper methods
-    
+
     def get_user_channels(self):
         return list(Channel.objects.filter(members=self.user))
+
+    async def send_error(self, message):
+        await self.send(text_data=json.dumps({
+            'event': 'error',
+            'data': {
+                'message': message
+            }
+        }))
