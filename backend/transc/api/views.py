ce08b55ca20bebe9f8b2c9f31cfb7678228f66da
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-01-24 18:52:30 +0100

    Added logout to invalidate session

--- backend/transc/api/views.py
+++ backend/transc/api/views.py
@@ -1,6 +1,8 @@
 from django.views import View
 from django.http import JsonResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.utils.decorators import method_decorator
+from . import bridge_websocket as websocket
 from .decorators import *
 #from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 
@@ -12,7 +14,7 @@ def index(request):
 def websocket_log(request): # TODO: DEBUG: Remove later
 	return render(request, 'api/ws.html')
 
-def websocket(request): # TODO: DEBUG: Remove later
+def websocket_custom(request): # TODO: DEBUG: Remove later
 	return render(request, 'api/custom_ws.html')
 
 # Endpoint: /login
@@ -24,4 +26,12 @@ class Login(View):
 		if user is None:
 			return JsonResponse({ERROR_FIELD: "Invalid login credentials"}, status=401)
 		login(request, user)
-		return JsonResponse(user.serialize())
+		return JsonResponse(user.serialize(private=True))
+
+# Endpoint: /logout
+@method_decorator(login_required, name='dispatch')
+class Logout(View):
+	def post(self, request):
+		# TODO: Implement disconnecting from websockets
+		logout(request)
+		return JsonResponse({'response': "Successfully logged out"})
