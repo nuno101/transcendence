b39cd774fd4da1300ffaa7ce91f5d085c56b82a9
jharrach <jharrach@student.42heilbronn.de>
2024-01-29 15:21:51 +0100

    nav highlight current view

--- frontend/src/components/common/Header.vue
+++ frontend/src/components/common/Header.vue
@@ -3,23 +3,25 @@
     <header class="p-3 text-bg-dark">
       <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-          </a>
-
           <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            <li v-for="e in navRoutes" :key="e.route">
+              <router-link :to="'/' + e.route" :class="[routeName === e.route ? activeView : inactiveView]">{{ e.button }}</router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" :class="[gameRoutes.includes(routeName) ? activeView : inactiveView]" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Game</a>
+              <ul class="dropdown-menu">
+                <li><router-link to="/game/online" class="dropdown-item">online</router-link></li>
+                <li><router-link to="/game/onsite" class="dropdown-item">onsite</router-link></li>
+              </ul>
+            </li>
           </ul>
-
+          
           <div v-if="logged">
-            <button type="button" class="btn btn-outline-light me-2"><router-link to="/profile">Profile</router-link></button>
+            <button type="button" class="btn btn-outline-light me-2">
+              <router-link to="profile" class="nav-link px-2 text-white">Profile</router-link>
+            </button>
             <button @click="LogOut" type="button" class="btn btn-secondary">Logout</button>
           </div>
-
           <div v-else class="text-end">
             <button type="button" class="btn btn-outline-light me-2" data-bs-target="#loginModalToggle" data-bs-toggle="modal">Login</button>
             <button type="button" class="btn btn-secondary" data-bs-target="#signupModalToggle" data-bs-toggle="modal">Signup</button>
@@ -34,12 +36,37 @@
 
 <script setup>
 import Backend from "../../js/Backend"
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import router from '../../router'
 import Login from '../auth/Login.vue'
 import Signup from '../auth/Signup.vue'
+import { useRoute } from 'vue-router'
 
 let logged = ref(false)
+
+const inactiveView = {
+  'nav-link': true,
+  'px-2': true,
+  'text-white': true
+}
+
+const activeView = {
+  'nav-link': true,
+  'px-2': true,
+  'text-secondary': true
+}
+
+const route = useRoute()
+let routeName = ref(null)
+const navRoutes = [
+  { route: 'dashboard', button: 'Dashboard' },
+  { route: 'friends', button: 'Friends' },
+  { route: 'settings', button: 'Settings' },
+  { route: 'tournaments', button: 'Tournaments' },
+]
+const gameRoutes = ['game/online', 'game/onsite', 'ponggame']
+
+watch(route, async (newroute) => routeName.value = newroute.name)
 
 const LogOut = async () => {
   try {
@@ -53,8 +80,4 @@ const LogOut = async () => {
 </script>
 
 <style scoped>
-  a {
-   text-decoration: none;
-   color: unset;
-  }
 </style>
