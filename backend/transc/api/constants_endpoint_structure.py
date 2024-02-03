963f71c013e25154ee575f13440c0b4f8f228c5b
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-02-03 15:57:04 +0100

    Changed structure file to use tabs instead of spaces for indentation

--- backend/transc/api/constants_endpoint_structure.py
+++ backend/transc/api/constants_endpoint_structure.py
@@ -3,359 +3,359 @@
 BODY_METHODS = ["POST", "PATCH"]
 
 NO_PARAM_METHOD_TEMPLATE = {
-  "query_params": {},
-  "body_params": {},
+	"query_params": {},
+	"body_params": {},
 }
 
 ENDPOINTS = {
-  "/login": {
-    "methods": {
-      "POST": {
-        "content_type": "application/json", 
-        "query_params": {
-          "remember": {
-            "type": "boolean",
-            "required": False,
-            "default": False,
-            "description": "If false, the session will be deleted after the browser is closed"
-          }
-        },
-        "body_params": {
-          "username": {
-            "type": "string",
-            "required": True,
-            "description": "Username of the user"
-          }, 
-          "password": {
-            "type": "string",
-            "required": True,
-            "description": "Password of the user"
-          }
-        },
-      }
-    }
-  }, "/logout": {
-    "methods": {
-      "POST": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "PATCH": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "nickname": {
-            "type": "string",
-            "required": False,
-            "description": "Nickname of the user"
-          }, "password": {
-            "type": "string",
-            "required": False,
-            "description": "Password of the user"
-          }
-        },
-      }, "DELETE": NO_PARAM_METHOD_TEMPLATE
-    }
-  }, "/users/me/avatar": {
-    "methods": {
-      "POST": {
-        # TODO: Specify parameters
-        "query_params": {},
-        "body_params": [],
-      }
-    }
-  }, "/users/me/blocked": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "user_id": {
-            "type": "id",
-            "required": True,
-            "description": "ID of the user to block"
-          }
-        },
-      }
-    }
-  }, "/users/me/blocked/USER_ID": {
-    "methods": {
-      "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me/channels": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me/friends": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me/friends/USER_ID": {
-    "methods": {
-      "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me/friends/requests": {
-    "methods": {
-      "GET": {
-        "query_params": {
-          "type": {
-            "type": "string",
-            "required": False,
-            "default": "all",
-            "description": "Type of the friend requests returned (all, sent, received)"
-          }
-        },
-        "body_params": [],
-      }, "POST": {
-        "content_type": "application/json", 
-        "query_params": {},
-        "body_params": {
-          "username": {
-            "type": "string",
-            "required": True,
-            "description": "Username of the user to send the friend request to"
-          }
-        },
-      }
-    }
-  }, "/users/me/friends/requests/REQUEST_ID": {
-    "methods": {
-      "POST": NO_PARAM_METHOD_TEMPLATE,
-      "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me/notifications": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/me/notifications/NOTIFICATION_ID": {
-    "methods": {
-      "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "username": {
-            "type": "string",
-            "required": True,
-            "description": "Username of the user"
-          }, "password": {
-            "type": "string",
-            "required": True,
-            "description": "Password of the user"
-          }
-        },
-      }
-    }
-  }, "/users/USER_ID": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "PATCH": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "nickname": {
-            "type": "string",
-            "required": False,
-            "description": "Nickname of the user"
-          }, "password": {
-            "type": "string",
-            "required": False,
-            "description": "Password of the user"
-          }
-        },
-      }, "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/USER_ID/avatar": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/USER_ID/stats": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/users/USER_ID/games": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/tournaments": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "title": {
-            "type": "string",
-            "required": True,
-            "description": "Title of the tournament"
-          }, "description": {
-            "type": "string",
-            "required": False,
-            "description": "Description of the tournament"
-          }
-        },
-      }
-    }
-  }, "/tournaments/TOURNAMENT_ID": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "PATCH": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "title": {
-            "type": "string",
-            "required": False,
-            "description": "Title of the tournament"
-          }, "description": {
-            "type": "string",
-            "required": False,
-            "description": "Description of the tournament"
-          }
-        },
-      }
-    }
-  }, "/games": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "tournament_id": {
-            "type": "id",
-            "required": False,
-            "description": "ID of the tournament"
-          }, "player1_id": {
-            "type": "id",
-            "required": True,
-            "description": "ID of the first player"
-          }, "player2_id": {
-            "type": "id",
-            "required": True,
-            "description": "ID of the second player"
-          }, "player1_score": {
-            "type": "integer",
-            "required": True,
-            "description": "Score of the first player"
-          }, "player2_score": {
-            "type": "integer",
-            "required": True,
-            "description": "Score of the second player"
-          }
-        },
-      }
-    }
-  }, "/games/GAME_ID": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "PATCH": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "player1_score": {
-            "type": "integer",
-            "required": False,
-            "description": "Score of the first player"
-          }, "player2_score": {
-            "type": "integer",
-            "required": False,
-            "description": "Score of the second player"
-          }, "status": {
-            "type": "string",
-            "required": False,
-            "description": "Status of the game"
-          }
-        },
-      }, "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/channels": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "name": {
-            "type": "string",
-            "required": True,
-            "description": "Name of the channel"
-          }
-        },
-      }
-    }
-  }, "/channels/CHANNEL_ID": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "PATCH": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "name": {
-            "type": "string",
-            "required": False,
-            "description": "Name of the channel"
-          }
-        },
-      }, "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/channels/CHANNEL_ID/members": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "user_id": {
-            "type": "id",
-            "required": True,
-            "description": "ID of the user to add to the channel"
-          }
-        },
-      }
-    }
-  }, "/channels/CHANNEL_ID/members/USER_ID": {
-    "methods": {
-      "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/channels/CHANNEL_ID/messages": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-      "POST": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "content": {
-            "type": "string",
-            "required": True,
-            "description": "Content of the message"
-          }
-        },
-      }
-    }
-  }, "/messages": {
-    "methods": {
-      "GET": NO_PARAM_METHOD_TEMPLATE,
-    }
-  }, "/messages/MESSAGE_ID": {
-    "methods": {
-      "PATCH": {
-        "content_type": "application/json",
-        "query_params": {},
-        "body_params": {
-          "content": {
-            "type": "string",
-            "required": True,
-            "description": "Content of the message"
-          }
-        },
-      }, "DELETE": NO_PARAM_METHOD_TEMPLATE,
-    }
-  } 
+	"/login": {
+		"methods": {
+			"POST": {
+				"content_type": "application/json", 
+				"query_params": {
+					"remember": {
+						"type": "boolean",
+						"required": False,
+						"default": False,
+						"description": "If false, the session will be deleted after the browser is closed"
+					}
+				},
+				"body_params": {
+					"username": {
+						"type": "string",
+						"required": True,
+						"description": "Username of the user"
+					}, 
+					"password": {
+						"type": "string",
+						"required": True,
+						"description": "Password of the user"
+					}
+				},
+			}
+		}
+	}, "/logout": {
+		"methods": {
+			"POST": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"PATCH": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"nickname": {
+						"type": "string",
+						"required": False,
+						"description": "Nickname of the user"
+					}, "password": {
+						"type": "string",
+						"required": False,
+						"description": "Password of the user"
+					}
+				},
+			}, "DELETE": NO_PARAM_METHOD_TEMPLATE
+		}
+	}, "/users/me/avatar": {
+		"methods": {
+			"POST": {
+				# TODO: Specify parameters
+				"query_params": {},
+				"body_params": [],
+			}
+		}
+	}, "/users/me/blocked": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"user_id": {
+						"type": "id",
+						"required": True,
+						"description": "ID of the user to block"
+					}
+				},
+			}
+		}
+	}, "/users/me/blocked/USER_ID": {
+		"methods": {
+			"DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me/channels": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me/friends": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me/friends/USER_ID": {
+		"methods": {
+			"DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me/friends/requests": {
+		"methods": {
+			"GET": {
+				"query_params": {
+					"type": {
+						"type": "string",
+						"required": False,
+						"default": "all",
+						"description": "Type of the friend requests returned (all, sent, received)"
+					}
+				},
+				"body_params": [],
+			}, "POST": {
+				"content_type": "application/json", 
+				"query_params": {},
+				"body_params": {
+					"username": {
+						"type": "string",
+						"required": True,
+						"description": "Username of the user to send the friend request to"
+					}
+				},
+			}
+		}
+	}, "/users/me/friends/requests/REQUEST_ID": {
+		"methods": {
+			"POST": NO_PARAM_METHOD_TEMPLATE,
+			"DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me/notifications": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/me/notifications/NOTIFICATION_ID": {
+		"methods": {
+			"DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"username": {
+						"type": "string",
+						"required": True,
+						"description": "Username of the user"
+					}, "password": {
+						"type": "string",
+						"required": True,
+						"description": "Password of the user"
+					}
+				},
+			}
+		}
+	}, "/users/USER_ID": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"PATCH": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"nickname": {
+						"type": "string",
+						"required": False,
+						"description": "Nickname of the user"
+					}, "password": {
+						"type": "string",
+						"required": False,
+						"description": "Password of the user"
+					}
+				},
+			}, "DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/USER_ID/avatar": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/USER_ID/stats": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/users/USER_ID/games": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/tournaments": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"title": {
+						"type": "string",
+						"required": True,
+						"description": "Title of the tournament"
+					}, "description": {
+						"type": "string",
+						"required": False,
+						"description": "Description of the tournament"
+					}
+				},
+			}
+		}
+	}, "/tournaments/TOURNAMENT_ID": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"PATCH": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"title": {
+						"type": "string",
+						"required": False,
+						"description": "Title of the tournament"
+					}, "description": {
+						"type": "string",
+						"required": False,
+						"description": "Description of the tournament"
+					}
+				},
+			}
+		}
+	}, "/games": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"tournament_id": {
+						"type": "id",
+						"required": False,
+						"description": "ID of the tournament"
+					}, "player1_id": {
+						"type": "id",
+						"required": True,
+						"description": "ID of the first player"
+					}, "player2_id": {
+						"type": "id",
+						"required": True,
+						"description": "ID of the second player"
+					}, "player1_score": {
+						"type": "integer",
+						"required": True,
+						"description": "Score of the first player"
+					}, "player2_score": {
+						"type": "integer",
+						"required": True,
+						"description": "Score of the second player"
+					}
+				},
+			}
+		}
+	}, "/games/GAME_ID": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"PATCH": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"player1_score": {
+						"type": "integer",
+						"required": False,
+						"description": "Score of the first player"
+					}, "player2_score": {
+						"type": "integer",
+						"required": False,
+						"description": "Score of the second player"
+					}, "status": {
+						"type": "string",
+						"required": False,
+						"description": "Status of the game"
+					}
+				},
+			}, "DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/channels": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"name": {
+						"type": "string",
+						"required": True,
+						"description": "Name of the channel"
+					}
+				},
+			}
+		}
+	}, "/channels/CHANNEL_ID": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"PATCH": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"name": {
+						"type": "string",
+						"required": False,
+						"description": "Name of the channel"
+					}
+				},
+			}, "DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/channels/CHANNEL_ID/members": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"user_id": {
+						"type": "id",
+						"required": True,
+						"description": "ID of the user to add to the channel"
+					}
+				},
+			}
+		}
+	}, "/channels/CHANNEL_ID/members/USER_ID": {
+		"methods": {
+			"DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/channels/CHANNEL_ID/messages": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+			"POST": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"content": {
+						"type": "string",
+						"required": True,
+						"description": "Content of the message"
+					}
+				},
+			}
+		}
+	}, "/messages": {
+		"methods": {
+			"GET": NO_PARAM_METHOD_TEMPLATE,
+		}
+	}, "/messages/MESSAGE_ID": {
+		"methods": {
+			"PATCH": {
+				"content_type": "application/json",
+				"query_params": {},
+				"body_params": {
+					"content": {
+						"type": "string",
+						"required": True,
+						"description": "Content of the message"
+					}
+				},
+			}, "DELETE": NO_PARAM_METHOD_TEMPLATE,
+		}
+	} 
 }
