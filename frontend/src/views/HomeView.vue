dc67d00ca18396e4423a06674fffd00b2d12f727
jharrach <jharrach@student.42heilbronn.de>
2024-02-07 17:17:16 +0100

    restored HomeView.vue file

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,10 @@
 <script setup>
 import AuthChoice from '../components/auth/AuthChoice.vue';
-import Loading from '../components/common/Loading.vue'
-import { ref } from 'vue'
-
-const loaded = ref(false)
-
-const foo = async () => {
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  loaded.value = true
-}
-foo()
-
 </script>
 
 <template>
   <main>
-    <AuthChoice v-if="loaded" />
-    <Loading v-else />
+    <AuthChoice />
   </main>
 </template>
 
