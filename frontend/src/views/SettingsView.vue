82c843eb2eacfb42e206561e8953bd563f1a042e
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-02-08 12:52:57 +0100

    SettingsView nickname & pwd changeable

--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -3,9 +3,11 @@ import { useI18n } from 'vue-i18n';
 import Backend from '../js/Backend';
 import { ref, onMounted } from 'vue';
 
+const input = ref({ nickname: '', password: '' })
+const password2 = ref('');
 const user = ref([]);
-const newNickname = ref('');
 let isUnique = ref(true);
+let successful = ref(0); // 0 nothing changed, 1 nickname, 2 password, 3 both
 
 onMounted(() => {
   fetchData();
@@ -21,10 +23,24 @@ const fetchData = async () => {
 
 const submitChanges = async() => {
   try {
-    await Backend.patch(`/api/users/me`, {"nickname": `${newNickname.value}`});
-    user.value.nickname = newNickname.value;
-    newNickname.value = '';
-    isUnique.value = true;
+    successful.value = 0;
+    if(input.value.password !== '' && input.value.nickname !== '') {
+      await Backend.patch(`/api/users/me`, {"nickname": `${input.value.nickname}`, "password": `${input.value.password}`});
+      user.value.nickname =  input.value.nickname;
+      password2.value = input.value.password = input.value.nickname = '';
+      isUnique.value = true;
+      successful.value = 3;
+    } else if (input.value.nickname !== ''){
+      await Backend.patch(`/api/users/me`, {"nickname": `${input.value.nickname}`});
+      user.value.nickname =  input.value.nickname;
+      input.value.nickname = '';
+      isUnique.value = true;
+      successful.value = 1;
+    } else if (input.value.password !== '') {
+      await Backend.patch(`/api/users/me`, {"password": `${input.value.password}`});
+      password2.value = input.value.password = '';
+      successful.value = 2;
+    }
   } catch (err) {
     console.log(err.message);
     if (err.message === "Bad Request")
@@ -55,34 +71,32 @@ const submitChanges = async() => {
               <div class="form-group row mt-3 align-items-center">
                   <label class="col-md-3 col-sm-4 col-xs-12 control-label">Nickname</label>
                   <div class="col-md-9 col-sm-8 col-xs-12">
-                      <input type="text" class="form-control" :placeholder="user.nickname" v-model="newNickname">
+                      <input type="text" class="form-control" :placeholder="user.nickname" v-model="input.nickname">
                       <div v-if="!isUnique" class="p-2 mt-1 alert alert-danger" role="alert">
                         Nickname is already taken
                       </div>
                   </div>
               </div>
-              <div class="form-group row mt-3">
+              <div class="form-group row mt-3 align-items-center">
                   <label class="col-md-3 col-sm-4 col-xs-12 control-label">Password</label>
                   <div class="col-md-9 col-sm-8 col-xs-12">
-                      <!-- <input type="password" class="mt-1 form-control" placeholder="old password"> -->
-                      <input type="password" class="mt-1 form-control" placeholder="new password">
-                      <input type="password" class="mt-1 form-control" placeholder="confirm new password">
+                      <input type="password" class="mt-1 form-control" placeholder="new password" v-model="input.password">
+                      <input type="password" class="mt-1 form-control" placeholder="confirm new password" v-model="password2">
+                      <div v-if="input.password !== password2" class="p-2 mt-1 alert alert-danger" role="alert">
+                        Passwords do not match
+                      </div>
                   </div>
               </div>
               <div class="mt-5 text-center text-sm-start">
-              <button type="button" class="btn btn-outline-primary" @click="submitChanges()">Update Profile</button>
+              <button type="button" class="btn btn-outline-primary" @click="submitChanges()" :disabled="input.password !== password2">Update Profile</button>
+              <div v-if="successful > 0" class="p-2 mt-1 alert alert-success" role="alert">
+                Successful change of {{ successful === 1 ? "nickname" : successful === 2 ? "password" : "nickname and password" }}
+              </div>
               </div>
             </div>
         </div>
       </div>
     </div>
-  <h1>{{useI18n().t('settingsview.editprofile')}}</h1>
-  <h3>{{useI18n().t('settingsview.nickname')}}</h3>
-  <p>(must be unique)</p>
-  <h1>{{useI18n().t('settingsview.avatar')}}</h1>
-  <h3>{{useI18n().t('settingsview.upload')}}/{{useI18n().t('settingsview.deletecurrent')}}</h3>
-  <p>(default option must be provided)</p>
-  <button type="button" class="btn btn-success">{{useI18n().t('settingsview.savechanges')}}</button>
 </template>
 
 <style>
