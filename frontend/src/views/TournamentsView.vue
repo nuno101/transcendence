25b2e2e6689cc46fbedd880b6163c43264a33923
Anouk <anouk.mechain@hotmail.fr>
2024-02-14 11:22:00 +0100

    fixing issues in view_tournament

--- frontend/src/views/TournamentsView.vue
+++ frontend/src/views/TournamentsView.vue
@@ -111,8 +111,8 @@ onMounted(() => {
         </div>
     <div>
     <button type="button" class="btn btn-primary" @click="openModal = !openModal">{{useI18n().t('tournamentsview.createatournament')}}</button>
-    <div v-show="openModal" class="modal-content">
-        <form @submit.prevent="submitForm">
+    <modal v-if="openModal" @close="openModal = false">
+        <form @submit.prevent="addNewTournament">
             <div class="form-group">
                 <label for="title">{{useI18n().t('tournamentsview.titleoftournament')}}</label>
                 <input type="text" class="form-control" id="title" placeholder="Enter title" v-model="input.title" required>
@@ -124,13 +124,13 @@ onMounted(() => {
             <br/>
             <div>
                 <button type="button" class="btn btn-danger" @click="resetInputFields">{{useI18n().t('tournamentsview.cancel')}}</button>
-                <button type="submit" class="btn btn-success" @click="addNewTournament">{{useI18n().t('tournamentsview.addtournament')}}</button>
+                <button type="submit" class="btn btn-success">{{useI18n().t('tournamentsview.addtournament')}}</button>
             </div>   
             <PostRequest v-if="submit" :apiPath="'/api/tournaments'" :data='formData'></PostRequest>
         </form >
+	</modal>
     </div >
   </div >
-</div>
 </template>
 
 <style>
