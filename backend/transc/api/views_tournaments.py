2a3f836b976c0e88f7789bf62648a021bf2aa858
Nuno <nlouro@student.42heilbronn.de>
2024-02-23 22:39:46 +0100

    Bugfixing

--- backend/transc/api/views_tournaments.py
+++ backend/transc/api/views_tournaments.py
@@ -64,23 +64,21 @@ class TournamentSingle(View):
 		if request.json.get('description') is not None:
 			tournament.description = request.json.get('description')
 		if request.json.get('status') is not None:
-			if request.json.get('status') == TournamentStatus.CANCELLED and \
-				tournament.status != TournamentStatus.DONE and \
-				tournament.status != TournamentStatus.CREATED and \
-				tournament.status != TournamentStatus.ONGOING:
+			if request.json.get('status') == "cancel" and \
+				(tournament.status != TournamentStatus.DONE or \
+				tournament.status != TournamentStatus.CREATED or \
+				tournament.status != TournamentStatus.CANCELLED):
 				tournament.status = TournamentStatus.CANCELLED
-			elif request.json.get('status') == "next":
+			# status done can not be triggered manually.
+			# completion of all games will change tournament.status to done.
+			elif request.json.get('status') == "next" and (tournament.status != TournamentStatus.CANCELLED or tournament.status != TournamentStatus.DONE):
 				next_status = TournamentStatus.states[TournamentStatus.states.index(tournament.status) + 1]
 				if next_status == "registration_closed":
 					if len(tournament.players.all()) < 2:
 						return JsonResponse({ERROR_FIELD: "Not enough players"}, status=400)
-					else:
-						# Schedule games to be played. Scores are set to 0 by default
+					else: # Schedule games to be played. Scores are set to 0 by default
 						create_games(tournament)
-				# status done can not be triggered manually.
-				# completion of all games will change tournament.status to done.
-				elif next_status != "cancel" or next_status != "done":
-					tournament.status = next_status
+				tournament.status = next_status
 			else:
 				return JsonResponse({ERROR_FIELD: "Invalid status"}, status=400)
 
