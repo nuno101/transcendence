652d00daaf5e08e704b6333ae24d24026fe4aa28
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-03-02 17:37:14 +0100

    Moved ws event handler function into separate file

---
+++ frontend/src/js/event_handler/MainHandler.js
@@ -0,0 +1,17 @@
+import Notifications from '../Notifications'
+
+// TODO: Add structure to define a function/event handler for each event that is being
+// by the websocket event handler
+
+export async function mainHandler(event) {
+    console.log(event)
+
+    // TODO: Use Structure here to decide which function to forward the event to
+
+    // TODO: Instead of checking here if the page reload message should be displayed move this
+    // functionality into the event handlers for specific events and sites
+    // Post Event if event happens not on current page
+    if(!Notifications.checkPageAndEvents(window.location.pathname, JSON.parse(event.data).event))
+        await Notifications.postNotification(event.data);
+}
+// ----------------------------------------------------------------------------------------------------
