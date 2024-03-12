ba0b0130d71f3a5df86c798f9a6d137db9815b3f
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-03-12 18:51:53 +0100

    added link to user profile & tournament title to UserStats

--- frontend/src/components/common/UserRow.vue
+++ frontend/src/components/common/UserRow.vue
@@ -12,17 +12,25 @@ const props = defineProps({
   bgColor: {
     type: String,
     default: "bg-light"
+  },
+  linkColor: {
+    type: String,
+    default: ""
+  },
+  avatarStyle: {
+    type: String,
+    default: "d-none d-lg-table-cell align-middle text-end"
   }
 });
 </script>
 
 <template>
     <td class="d-lg-none" :class="bgColor"></td>
-    <td class="d-none d-lg-table-cell align-middle" :class="bgColor">
+    <td :class="[avatarStyle, bgColor]">
         <GetAvatar :id="props.user.id" :size="props.size" />
     </td>
     <td class="text-start align-middle" :class="bgColor">
         <OnlineStatus :status="props.user.status" :id="props.user.id" class="me-2"/>
-        <router-link :to="`/users/${props.user.id}`">{{props.user.nickname}}</router-link>
+        <router-link :class="linkColor" :to="`/users/${props.user.id}`">{{props.user.nickname}}</router-link>
     </td>
 </template>
