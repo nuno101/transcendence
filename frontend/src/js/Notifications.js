68abe136dd842dae71009ebb0a9fc61ff706d9be
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-03-17 20:32:42 +0100

    notifications translations done

--- frontend/src/js/Notifications.js
+++ frontend/src/js/Notifications.js
@@ -1,3 +1,4 @@
+import { useI18n } from 'vue-i18n';
 import { ref } from 'vue';
 import Backend from './Backend';
 
@@ -11,24 +12,25 @@ const parser = {
 //        different websocket events
 // ----------------------------------------------------------------------------
 
+// useI18n().t('settings.nickname')
 async function newMessage(data) {
     return {
         type: data.event,
-        content: "Go to chat to see new messages"
+        content: "newMessage"
     }
 }
 
 async function newFriendRequest(data) {
     return {
         type: data.event,
-        content: "Go to friends to see new friend request"
+        content: "newFriendRequest"
     }
 }
 
 async function newFriend(data) {
     return {
         type: data.event,
-        content: "Go to friends to see new friend"
+        content: "newFriends"
     }
 }
 // ----------------------------------------------------------------------------
