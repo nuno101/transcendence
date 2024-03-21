e0228f3333e42116bc54fd3d6e45bd1825485c37
Julia <jmatheis@student.42heilbronn.de>
2024-03-21 10:12:11 +0100

    Update frontend/src/components/auth/PlayerGameAuth.vue
    Co-authored-by: jharrach <105501741+jharrach@users.noreply.github.com>

--- frontend/src/components/auth/PlayerGameAuth.vue
+++ frontend/src/components/auth/PlayerGameAuth.vue
@@ -4,7 +4,7 @@
 			<div class="modal-content rounded-4 shadow">
 				<div class="modal-header p-5 pb-4 border-bottom-0">
 					<h1 class="fw-bold mb-0 fs-2"  id="playerAuthToggleLabel">{{useI18n().t('auth.authentication')}}</h1>
-					<button @click="closeModal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 				</div>
 				<div v-for="(player, index) in authPlayers" :key="index" :class="['modal-body', 'p-5', 'pt-0', { 'py-0': player.isAuthenticated }]">
 					<div v-for="alert in player.alerts" :class="alert.type">
