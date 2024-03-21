d0a4c7c88bf8747923944adfdb601f1fc007f00a
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-03-21 10:06:47 +0100

    bg image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,31 @@
 <script setup>
 import LanguageSelection from './components/common/LanguageSelection.vue'
 import Notifications from '../src/js/Notifications'
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
 //import './colors.css' - TODO: remove file?
+
+// const bg = ref(null);
+const router = useRouter();
+
+// Navigation guard to handle background image change based on route
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') {
+    // Change the background image for the root route
+    bg.value = './assets/background_root.png';
+  } else {
+    // Use the default background image for other routes
+    bg.value = './assets/background.png';
+  }
+  next();
+});
+
 </script>
 
 <!-- add things here that should be visible on ALL sites -->
 <template>
+	<div class="background-image">
 	<LanguageSelection />
 	<RouterView name="Header" />
 	<div class="container py-4 px-3 mx-auto">
@@ -15,4 +35,19 @@ import Notifications from '../src/js/Notifications'
 		</div>
 		<RouterView />
 	</div>
+	</div>
 </template>
+
+<style scoped>
+.background-image {
+  /* Adjust the URL to the path of your background image */
+  background-image: url('./assets/background.png');
+  /* Set background size and other properties as needed */
+  background-size: cover;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -15,25 +15,19 @@ const profileLinks = [
 <template>
   <div>
     <UpcomingGames/>
-    <div class="d-flex justify-content-center align-items-center mt-3">
-      <div class="text-center">
-        <h1 class="m-0 p-0">PONG GAME</h1>
-        <h5>{{useI18n().t('home.ourTranscendenceProject')}}</h5>
-        <p>{{useI18n().t('home.transcendenceDescription')}}</p>
-      </div>
-    </div>
-  <footer class="container-fluid bg-light fixed-bottom p-3 text-center" style="z-index: 1;">
+  <footer class="container-fluid fixed-bottom p-3 text-center text-white" style="z-index: 1; background-color: rgba(0, 0, 0, 0.5);">
       <h6 class="text-uppercase fw-bold pb-1">{{useI18n().t('home.connectOnGithub')}}</h6>
       <div class="d-flex flex-wrap justify-content-center">
         <p class="mx-md-4 mx-2" v-for="(link, index) in profileLinks" :key="index">
-          <i class="bi bi-github"></i> <a class="link-opacity-50-hover" :href="link.link">{{ link.name }}</a>
+          <i class="bi bi-github"></i> <a class="link-opacity-50-hover text-white" :href="link.link">{{ link.name }}</a>
         </p>
       </div>
     <div>
-      <i class="bi bi-github"></i> <a class="link-opacity-50-hover" href="https://github.com/nuno101/transcendence">{{useI18n().t('home.ourTranscendenceProject')}}</a>
+      <i class="bi bi-github"></i> <a class="link-opacity-50-hover text-white" href="https://github.com/nuno101/transcendence">{{useI18n().t('home.ourTranscendenceProject')}}</a>
     </div>
   </footer>
   </div>
 </template>
 
-<style></style>
+<style>
+</style>
