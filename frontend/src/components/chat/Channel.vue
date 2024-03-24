8877933a03265b34ed26db21142d34ef5ebce1d8
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-03-24 12:26:25 +0100

    Update frontend/src/components/chat/Channel.vue
    Co-authored-by: jharrach <105501741+jharrach@users.noreply.github.com>

--- frontend/src/components/chat/Channel.vue
+++ frontend/src/components/chat/Channel.vue
@@ -12,7 +12,7 @@ const props = defineProps({
   <li :class="{ 'active': selected }" class="channel-item list-group-item list-group-item-action py-3 lh-sm"
     @click="$emit('selected')">
     <div class="d-flex w-100 justify-content-between align-items-center">
-      <h5 class="mb-1">{{ channel.name }}</h5>
+      <h1 class="mb-1 fs-5">{{ channel.name }}</h1>
     </div>
     <div class="d-flex justify-content-between align-items-center">
       <small>{{ formatTimestamp(channel.updated_at) }}</small>
