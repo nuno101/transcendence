121e098bcdcc96b97bde057507282f44ae42e83d
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-04-08 16:01:07 +0200

    improved onsite/local Game view

--- frontend/src/views/OnsiteGameView.vue
+++ frontend/src/views/OnsiteGameView.vue
@@ -1,19 +1,28 @@
 <script setup>
 import { useI18n } from 'vue-i18n';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import PlayerGameAuth from '../components/auth/PlayerGameAuth.vue';
 import { globalUser } from '../main';
 
 const auth = ref(null);
 
+onMounted(() => {
+	if(auth.value)
+		auth.value.openModal();
+})
 </script>
 
 <template>
-    <button type="button" class="btn btn-outline-light" @click="auth.openModal()">{{useI18n().t('onsite.authenticateSecondPlayer')}}</button>
-    <PlayerGameAuth
-      ref="auth"
-      :player1="globalUser"
-    />
+	<div class="text-center" style="margin-top: 20vh">
+		<button type="button"
+			class="btn btn-outline-light"
+			@click="auth.openModal()">{{useI18n().t('onsite.authenticateSecondPlayer')}}
+		</button>
+	</div>
+	<PlayerGameAuth
+		ref="auth"
+		:player1="globalUser"
+	/>
 </template>
 
 <style>
