0c36057871517645d9f288c4d2a768bbeb0c4432
jmatheis00 <jmatheis@student.42heilbronn.de>
2024-04-16 12:37:37 +0200

    channel name fix

--- frontend/src/components/chat/Channel.vue
+++ frontend/src/components/chat/Channel.vue
@@ -13,7 +13,7 @@ const props = defineProps({
   <li :class="{ 'active': selected }" class="channel-item list-group-item list-group-item-action py-3 lh-sm"
     @click="$emit('selected')">
     <div class="d-flex w-100 justify-content-between align-items-center">
-      <h1 class="mb-1 fs-5">{{ props.channel.members[0].username = globalUser.username ?
+      <h1 class="mb-1 fs-5">{{ props.channel.members[0].username === globalUser.username ?
         props.channel.members[1].nickname : props.channel.members[0].nickname }}</h1>
     </div>
     <div class="d-flex justify-content-between align-items-center">
