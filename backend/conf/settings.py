967e3e2d4aa14f88216e3440611ac4e7142aefd7
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2024-04-16 10:37:31 +0200

    Enabled django CRSF middleware

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,12 +49,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #FIXME - CSRF protection disabled
-	#'django.middleware.csrf.CsrfViewMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.LoggedInCheckMiddleware',
+    'api.middleware.JsonSyntaxCheckMiddleware',
 ]
 
 ROOT_URLCONF = 'transc.urls'
@@ -86,7 +89,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER', 'dummy'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'dummy'),
         'HOST': os.environ.get('POSTGRES_HOST', 'dummy'),  # or the IP address of your database server
-        #'PORT': os.environ.get('POSTGRES_PORT', '5432'),    # leave it empty to use the default port
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),    # leave it empty to use the default port
     }
 }
 
@@ -131,7 +134,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Custom user model
+
 AUTH_USER_MODEL = 'api.User'
+
+# Django channels settings
 
 ASGI_APPLICATION = 'transc.asgi.application'
 
@@ -139,7 +146,48 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [(os.environ.get("REDIS_HOST"), 6379)],
         },
     },
 }
+
+# Media settings
+
+MEDIA_ROOT = '/data/media/'
+
+MEDIA_URL = os.environ.get('DJANGO_MEDIA_URL', '/media/')
+
+# Logging settings
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'api': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
+# Language settings
+LANGUAGES = [
+    ("en", _("English")),
+    ("fr", _("French")),
+    ("es", _("Spanish")),
+    ("de", _("German")),
+]
--- backend/transc/transc/settings.py
+++ backend/transc/transc/settings.py
@@ -51,8 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #FIXME - CSRF protection disabled
-	#'django.middleware.csrf.CsrfViewMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware', #FIXME - CSRF protection disabled
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
