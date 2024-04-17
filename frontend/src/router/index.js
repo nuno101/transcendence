3aa7a41644f31e09efe1e84a2ac6711171466722
jharrach <jharrach@student.42heilbronn.de>
2024-04-17 11:07:18 +0200

    updated file name

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -39,7 +39,7 @@ const router = createRouter({
 			path: '/logout',
 			name: 'logout',
 			components: {
-				default: () => import('../views/Logout.vue'),
+				default: () => import('../views/LogoutView.vue'),
 				Header,
 			}
 		},
